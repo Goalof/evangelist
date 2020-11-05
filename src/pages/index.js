fc2e2b8de2e53c3418bb574174cf10794ed169f7
Goalof <68434460+Goalof@users.noreply.github.com>
2020-11-06 05:47:30 +0600

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Text, Input, Button } from "@quarkly/widgets";
+import { Theme, Link, Span, Text, Image, Box, Input, Button } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
-import { Section, Override, StackItem, Formspree, Stack } from "@quarkly/components";
+import { Override, StackItem, Formspree, Stack, Section } from "@quarkly/components";
 import * as Components from "components";
 export default (() => {
 	return <Theme theme={theme}>
@@ -14,7 +14,197 @@ export default (() => {
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
 		<Components.EmbedHTML />
-		<Section padding="120px 0 100px 0" background="rgba(0, 0, 0, 0) linear-gradient(0deg, rgb(14, 19, 23) 0%, rgb(34, 38, 46) 100%) repeat scroll 0% 0% / auto padding-box border-box">
+		<Section background="linear-gradient(180deg, #0E1317 0%, #22262E 100%)" padding="130px 0 130px 0">
+			<Text text-align="center" font="normal 400 90px/114px --fontFamily-sans" color="#ffffff" margin="16px 0px 100px 0px">
+				Мы ищем в команду quarkly{"  "}
+				<br />
+				<Span
+					overflow-wrap="normal"
+					word-break="normal"
+					white-space="normal"
+					text-indent="0"
+					text-overflow="clip"
+					hyphens="manual"
+					color="#66FFE4"
+				>
+					ИТ-евангелиста
+				</Span>
+				{"\n\n"}
+			</Text>
+			<Stack>
+				<StackItem
+					width="60%"
+					display="flex"
+					flex-direction="column"
+					padding="16px 0px 16px 16px"
+					border-radius="15px"
+				>
+					<Override
+						slot="StackItemContent"
+						flex-direction="column"
+						background="#22262E"
+						padding="26px 26px 26px 26px"
+						align-items="center"
+						border-radius="12px"
+					/>
+					<Box width="100%" display="flex">
+						<Image width="64px" height="64px" src="https://uploads.quarkly.io/5f37f93e5f5c20001ea9c99e/images/Group%20(25).svg?v=2020-11-05T23:46:00.699Z" />
+						<Text margin="16px 0px 16px 36px">
+							Some text
+						</Text>
+					</Box>
+					<Text font="normal 400 25px/37px --fontFamily-googleSourceSansPro" color="#ffffff">
+						Выстраивает и поддерживает качественные отношения с лидерами и авторитетами ИТ-сообщества.{" "}
+						<br />
+						<br />
+						Продвигает quarkly, нести ценность в массы.
+						<br />
+						<br />
+						Создавать великолепный контент.{" "}
+						<br />
+						<br />
+						Принимает активное участие в жизни quarkly через блоги, социальные сети, встречи, конференции, форумы и другие мероприятия.{" "}
+						<br />
+						<br />
+						Коммуницировать с блогерами, журналистами и редакторами тематических сайтов.{"\n\n"}
+					</Text>
+					{"        "}{"    "}
+				</StackItem>
+				<StackItem
+					width="40%"
+					display="flex"
+					flex-direction="column"
+					padding="16px 0px 16px 16px"
+					border-radius="15px"
+				>
+					<Override
+						slot="StackItemContent"
+						flex-direction="column"
+						background="#22262E"
+						padding="26px 6px 26px 6px"
+						align-items="center"
+						border-radius="12px"
+						justify-content="center"
+					/>
+					{"        "}
+					<Formspree errorMessage="Something went wrong" completeText="Success" endpoint="https://formspree.io/f/mpzypyye" width="70%">
+						<Input
+							display="flex"
+							flex-direction="column"
+							margin="0px 4px 20px 0px"
+							type="text"
+							border-radius="8px"
+							border-width="1px"
+							width="100%"
+							height="50px"
+							placeholder="NAME"
+							border-color="#000000"
+							background="#3F4750"
+							border-style="solid"
+							color="#ffffff"
+							font="normal 300 16px/1.5 --fontFamily-sans"
+							letter-spacing=".8PX"
+							hover-border-color="#66FFE4"
+							name="NAME"
+						/>
+						<Input
+							name="EMAIL"
+							display="flex"
+							flex-direction="column"
+							margin="0px 4px 20px 0px"
+							type="email"
+							border-radius="8px"
+							border-width="1px"
+							width="100%"
+							height="50px"
+							placeholder="EMAIL"
+							border-color="#000000"
+							background="#3F4750"
+							border-style="solid"
+							color="#ffffff"
+							font="normal 300 16px/1.5 --fontFamily-sans"
+							letter-spacing=".8PX"
+							hover-border-color="#66FFE4"
+							required
+						/>
+						<Input
+							name="Phone"
+							display="flex"
+							flex-direction="column"
+							margin="0px 4px 20px 0px"
+							type="text"
+							border-radius="8px"
+							border-width="1px"
+							width="100%"
+							height="50px"
+							background="#3F4750"
+							hover-border-color="#66FFE4"
+							color="#ffffff"
+							required
+							letter-spacing=".8PX"
+							placeholder="TELEGRAM"
+							border-color="#000000"
+						/>
+						<Input
+							name="Email"
+							display="flex"
+							flex-direction="column"
+							margin="0px 4px 20px 0px"
+							type="text"
+							border-radius="8px"
+							border-width="1px"
+							width="100%"
+							height="150px"
+							placeholder="✐ COMMENTS"
+							align-items="flex-start"
+							justify-content="start"
+							as="textarea"
+							border-color="#000000"
+							background="#3F4750"
+							hover-border-color="#66FFE4"
+							color="#ffffff"
+							letter-spacing=".8PX"
+						/>
+						<Button
+							display="flex"
+							background="#66FFE4"
+							font="normal 600 18px/1.5 --fontFamily-googleSourceSansPro"
+							letter-spacing="2px"
+							border-radius="8px"
+							padding="11px 42px"
+							color="#000000"
+							text-align="center"
+							width="100%"
+							justify-content="center"
+							hover-background="#000000"
+							hover-color="#ffffff"
+							transition="all 0.3s ease"
+						>
+							SEND
+						</Button>
+						<Text font="17px --fontFamily-googleSourceSansPro" color="--light" text-align="center">
+							By clicking the “SEND” button, you confirm that you have read and agree to the{" "}
+							<Link
+								href="https://quarkly.io/contest-terms"
+								overflow-wrap="normal"
+								word-break="normal"
+								white-space="normal"
+								text-indent="0"
+								text-overflow="clip"
+								hyphens="manual"
+								color="#66FFE4"
+							>
+								Contest Rules
+							</Link>
+							{"\n\n"}
+						</Text>
+					</Formspree>
+					{"    "}
+				</StackItem>
+				{"    "}{"    "}
+			</Stack>
+		</Section>
+		<Section padding="120px 0 120px 0" background="rgba(0, 0, 0, 0) linear-gradient(0deg, rgb(14, 19, 23) 0%, rgb(34, 38, 46) 100%) repeat scroll 0% 0% / auto padding-box border-box">
 			<Text font="normal 700 38px/1.2 --fontFamily-googleSourceSansPro" color="#ffffff" text-align="center">
 				Евангилиста продукта, который станет нашей частью на фул тайм или халф тайм, и сможет полюбить кваркли так как его любим мы, и выстраоить отношения с професиональным сообществом
 			</Text>
@@ -22,19 +212,7 @@ export default (() => {
 		<Section padding="30px 0 30px 0" background="--color-darkL1">
 			<Stack>
 				{"    "}{"    "}
-				<StackItem width="50%" display="flex" flex-direction="column" padding="16px 0px 16px 16px">
-					<Override
-						slot="StackItemContent"
-						flex-direction="column"
-						background="#22262E"
-						padding="36px 36px"
-						border-radius="12px"
-						align-items="center"
-						justify-content="center"
-					/>
-					{"        "}{"    "}
-				</StackItem>
-				<StackItem width="50%" display="flex" flex-direction="column" padding="16px 0px 16px 16px">
+				<StackItem width="100%" display="flex" flex-direction="column" padding="16px 0px 16px 16px">
 					<Override
 						slot="StackItemContent"
 						flex-direction="column"
